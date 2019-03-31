a23b4d4a506ab1a87b3861412ed9026c9ef831fb
Lucas Dai <yuanzhendai@gmail.com>
2019-03-31 21:16:22 +0800

    Add Config file for greedy

---
+++ template.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Lucas Dai <yuanzhendai@gmail.com>
+#
+import string, copy
+
+
+class ${ClassName}:
+    def ${Method.Name}(self, ${Method.Params}):
+        return ${Method.ReturnType;zeroval}
+
+
+${CutBegin}
+${<TestCode}
+${CutEnd}
